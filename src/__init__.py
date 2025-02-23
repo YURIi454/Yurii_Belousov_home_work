ec5a48f3e95d776029285c8969abc5e8063fb8b3
Yurii <yurii.1986.e@gmail.com>
2025-02-23 15:25:47 +0400

    На проверку. 11.1_generators

---
+++ src/__init__.py
@@ -0,0 +1,106 @@
+# from typing import Generator, Iterator
+#
+# transactions = [
+#     {
+#         "id": 939719570,
+#         "state": "EXECUTED",
+#         "date": "2018-06-30T02:08:58.425572",
+#         "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+#         "description": "Перевод организации",
+#         "from": "Счет 75106830613657916952",
+#         "to": "Счет 11776614605963066702",
+#     },
+#     {
+#         "id": 142264268,
+#         "state": "EXECUTED",
+#         "date": "2019-04-04T23:20:05.206878",
+#         "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+#         "description": "Перевод со счета на счет",
+#         "from": "Счет 19708645243227258542",
+#         "to": "Счет 75651667383060284188",
+#     },
+#     {
+#         "id": 873106923,
+#         "state": "EXECUTED",
+#         "date": "2019-03-23T01:09:46.296404",
+#         "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+#         "description": "Перевод со счета на счет",
+#         "from": "Счет 44812258784861134719",
+#         "to": "Счет 74489636417521191160",
+#     },
+#     {
+#         "id": 895315941,
+#         "state": "EXECUTED",
+#         "date": "2018-08-19T04:27:37.904916",
+#         "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+#         "description": "Перевод с карты на карту",
+#         "from": "Visa Classic 6831982476737658",
+#         "to": "Visa Platinum 8990922113665229",
+#     },
+#     {
+#         "id": 594226727,
+#         "state": "CANCELED",
+#         "date": "2018-09-12T21:27:25.241689",
+#         "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+#         "description": "Перевод организации",
+#         "from": "Visa Platinum 1246377376343588",
+#         "to": "Счет 14211924144426031657",
+#     },
+# ]
+#
+#
+# def filter_by_currency(data: list[dict], key_word: str) -> Iterator:
+#
+#     """Принимает на вход список словарей, представляющих транзакции.
+#     Возвращает итератор, который поочередно выдает транзакции, с заданной валютой."""
+#
+#     filtered_data = filter(lambda item: item["operationAmount"]["currency"]["code"] == key_word, data)
+#
+#     #print(type(transaction_descriptions(filtered_data)),"1")
+#
+#     return list(filtered_data)
+#
+# def transaction_descriptions(data: list) -> Generator[str]:
+#
+#     """Принимает список словарей с транзакциями и возвращает
+#     описание каждой операции по очереди."""
+#
+#     for descript_operation in data:
+#         #print(type(descript_operation["description"]), "2")
+#         return descript_operation["description"]
+#
+#         #print(type(descript_operation["description"]),"2")
+#
+#
+# def card_number_generator(start_number: int, stop_number: int) -> Generator[str]:
+#
+#         """Генератор номеров в формате XXXX XXXX XXXX XXXX
+#         в диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
+#
+#         finish_number = 9999_9999_9999_9999
+#
+#         if start_number > finish_number:
+#             return
+#
+#         if start_number < 1:
+#             start_number = 1
+#         if stop_number > finish_number:
+#             stop_number = finish_number
+#
+#         for number in range(start_number, stop_number + 1):
+#             str_number = str(number)
+#             str_number = str_number.rjust(16, "0")
+#
+#             output_number = []
+#             for item in range(0, 16, 4):
+#                 output_number.append(str_number[item: item + 4])
+#
+#                 card_number = " ".join(output_number)
+#
+#             yield card_number
+#
+#
+# print(filter_by_currency(transactions,"USD"))
+# print(filter_by_currency(transactions,"RUB"))
+# print(filter_by_currency([]," "))
+# print(list(card_number_generator(10090,10109)))
