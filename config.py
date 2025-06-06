bbac871a403919e19b18e57735dba7cd59bd0238
YURIi454 <yurii.1986.e@gmail.com>
2025-06-06 10:10:59 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ ROOT_DIR = os.path.dirname(__file__)
 LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
-PATH_JSON = 'C:/Users/Sergey/PycharmProjects/Yurii_Belousov/data/operations.json'
-PATH_CSV = 'C:/Users/Sergey/PycharmProjects/Yurii_Belousov/data/transactions.csv'
-PATH_XLSX = 'C:/Users/Sergey/PycharmProjects/Yurii_Belousov/data/transactions_excel.xlsx'
+PATH_JSON = ''
+PATH_CSV = ''
+PATH_XLSX = ''
